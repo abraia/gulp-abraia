2a8f660d3e0c7e64cf0c0a374b977665266cdcd1
Jorge Rodríguez-Araújo <jorge@abraiasoftware.com>
2019-01-19 17:37:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Abraia gulp plugin
+
+
+
+## License
+
+This software is licensed under the MIT License. [View the license](LICENSE).
---
+++ index.js
@@ -0,0 +1,28 @@
+const through = require('through2')
+const PluginError = require('plugin-error')
+const abraia = require('abraia/abraia')
+
+const PLUGIN_NAME = 'gulp-abraia'
+
+const gulpAbraia = () => {
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) return cb(null, file)
+    if (file.isBuffer()) {
+      // file.contents = file.contents
+      console.log('Buffer', file.path)
+      abraia.fromFile(file).toBuffer().then(data => {
+        console.log('Size', file.contents.length, data.length)
+        if (data.length < file.contents.length) {
+          file.contents = data
+        }
+        cb(null, file)
+      })
+    }
+    if (file.isStream()) {
+      console.log('Stream', file.path)
+      cb(null, file)
+    }
+  })
+}
+
+module.exports = gulpAbraia
